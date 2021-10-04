ceaeb5e1023844f3b9c5dd13623a30d58cbc9abe
unknown <jeremy.mcguirk@gmail.com>
2021-10-04 17:48:40 -0400

    got route to work. found the uuid package for node to help with unique ids when creating notes. still implementing how to get the notes into the db module.

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-// declaring that we're using express 
+const { notes } = require('./db/db.json')
+
+//Packages being used
+const fs = require('fs');
+
+// npm uuid
+// import { v4 as uuidv4 } from 'uuid';
+// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
+
+// declaring that we're using express
 const express = require('express');
 
 const path = require('path');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-// Middleware
-
+// Middleware (its looking at url encoded information and that its
+// using objects example:json)
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+
+   res.json(results);
+});
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
