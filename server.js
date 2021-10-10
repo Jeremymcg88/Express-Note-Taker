33bf34d03ca090a0b3a9a9c2606283bffccf9c64
unknown <jeremy.mcguirk@gmail.com>
2021-10-10 15:13:12 -0400

    Add Heroku

--- server.js
+++ server.js
@@ -1,32 +1,19 @@
-const { notes } = require('./db/db.json')
-
-//Packages being used
-const fs = require('fs');
-
-// npm uuid
-// import { v4 as uuidv4 } from 'uuid';
-// uuidv4(); // ⇨ '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d'
-
-// declaring that we're using express
 const express = require('express');
-
 const path = require('path');
-
-const PORT = process.env.PORT || 8080;
-
+const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoute = require('./Routes/apiRoutes/index');
+const htmlRoute = require('./Routes/htmlRoutes/index');
 
-// Middleware (its looking at url encoded information and that its
-// using objects example:json)
-app.use(express.urlencoded({extended: true}));
+// These set up the base code for the app
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-
-   res.json(results);
-});
+app.use(express.static(path.join(__dirname, "Develop/public")));
+// This displays the back-end information
+app.use("/api", apiRoute);
+// This displays the front-end information
+app.use("/", htmlRoute);
 
 app.listen(PORT, () => {
     console.log(`App listening on port: ${PORT}`);
-})
+});
