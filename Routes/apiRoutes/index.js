f84262ac464e133d0c6aeb551e055265687f5e5b
unknown <jeremy.mcguirk@gmail.com>
2021-10-19 15:03:53 -0400

    changed POST and GET to get information from DB correctly

--- Routes/apiRoutes/index.js
+++ Routes/apiRoutes/index.js
@@ -7,12 +7,9 @@ const abc = require('../../Develop/db/db.json')
 
 // GET method to display notes stored in db
 router.get("/notes", (req, res) => {
-<<<<<<< HEAD
-    fs.readFile("../../Develop/db/db.json", "utf8", function (err, data) {
-=======
     fs.readFile("./db/db.json", "utf8", function (err, data) {
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
-        let noteData = [];
+        let noteData = JSON.parse(data);
+        res.json(noteData.notes);
         if (err) {
             console.error(err)
             return
@@ -20,7 +17,7 @@ router.get("/notes", (req, res) => {
         console.log(data)
         if (data.length > 0) {
             noteData = JSON.parse(data);
-            res.json(noteData);
+            res.json(noteData.notes);
         } else {
             console.log("No notes saved");
         }
@@ -32,38 +29,22 @@ router.post("/notes", (req, res) => {
 
     console.log(newNote)
 
-<<<<<<< HEAD
-    fs.readFile("../../Develop/db/db.json", "utf8", (err, data) => {
-=======
     fs.readFile("./db/db.json", "utf8", (err, data) => {
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
         if (err) {
             console.log(`err at the database ${err}`);
         } else {
-            if (data.length > 0) {
-                obj = JSON.parse(data);
-            } else {
-                obj = [];
-            }
-            
+           let obj = JSON.parse(data);
+           
             newNote.id = v4().substring(0, 4);
-            obj.push(newNote);
+            obj.notes.push(newNote);
             
-<<<<<<< HEAD
-            fs.writeFile("../../Develop/db/db.json", JSON.stringify(obj), "utf8", (err) => {
-=======
             fs.writeFile("./db/db.json", JSON.stringify(obj), "utf8", (err) => {
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
                 if (err) {
                     throw err;
                 }
                 console.log("Note saved.");
                 
-<<<<<<< HEAD
-                fs.readFile("../../Develop/db/db.json", "utf8", function (err, data) {
-=======
                 fs.readFile("./db/db.json", "utf8", function (err, data) {
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
                     let noteData = [];
                     if (err) {
                         throw err;
@@ -81,36 +62,16 @@ router.post("/notes", (req, res) => {
     });
 });
 // DELETE method to delete notes stored in db
-<<<<<<< HEAD
-// router.delete("/notes/:id", (req, res) => {
-//     fs.readFile("../../Develop/db/db.json", "utf8", (err, data) => {
-//         if (err) {
-//             throw err;
-//         }
-//         let objNew = JSON.parse(data);
-=======
 router.delete("/notes/:id", (req, res) => {
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) {
             throw err;
         }
         let objNew = JSON.parse(data);
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
 
-//         const deleteThis = objNew.findIndex((note) => note.id === req.params.id);
-//         objNew.splice(deleteThis, 1);
+        const deleteThis = objNew.findIndex((note) => note.id === req.params.id);
+        objNew.splice(deleteThis, 1);
 
-<<<<<<< HEAD
-//         const output = fs.writeFile("../../Develop/db/db.json", JSON.stringify(objNew), (err) => {
-//             if (err) {
-//                 throw err;
-//             }
-//             console.log("Note rewritten");
-//         });
-//         res.send(output);
-//     });
-// });
-=======
         const output = fs.writeFile("./db/db.json", JSON.stringify(objNew), (err) => {
             if (err) {
                 throw err;
@@ -120,6 +81,5 @@ router.delete("/notes/:id", (req, res) => {
         res.send(output);
     });
 });
->>>>>>> 318c22e30d382f52b9a30ec4aeda1fd644ffb89f
 
 module.exports = router;
